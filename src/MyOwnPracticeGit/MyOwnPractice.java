5c12d8ffec0b312e856b5cd46d56c0c10fed6801
ArzuEs <117678815+ArzuEs@users.noreply.github.com>
2022-11-09 12:40:43 -0500

    Update MyOwnPractice.java
    This is from remote

--- src/MyOwnPracticeGit/MyOwnPractice.java
+++ src/MyOwnPracticeGit/MyOwnPractice.java
@@ -10,4 +10,9 @@ public class MyOwnPractice {
 	public void practice() {
 		System.out.println("this is for repository");
 	}
+	
+	public void practice2() {
+		System.out.println("this is from remote");
+	}
 }
+
